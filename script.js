8470614c46ce62aa9f8594868b3042b8c0545df9
sahithimandila <sahithi.mandila@gmail.com>
2026-01-22 16:07:50 +0530

    Add initial script.js with various JavaScript examples

---
+++ script.js
@@ -0,0 +1,128 @@
+/*console.log("Hello world");
+var a;
+a=10;
+console.log(a);
+const b= 20;
+//const b=20;
+console.log(b);
+let c=30;
+console.log(c);
+
+var a=100;//number
+console.log(str);
+var str="sai" // '' // `` //string
+console.log(str);
+var bool=true;//boolean
+console.log(bool);
+var un ;//undefined
+console.log(un);
+var nu=null;//null
+console.log(null);
+
+var big = "sai";
+Symbol=Symbol("li");
+console.log(typeof a);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof un);
+console.log(typeof nu);
+console.log(typeof big);
+console.log(typeof Symbol);*/
+
+//arthemetic operators
+
+/*var a=12;
+var b="20";
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//realational operators
+var a=10;
+var b=20;
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+
+//logical operators
+a=true;
+b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+
+var arr=[10,20,30,40];
+console.log(arr);
+console.log(typeof arr);
+ 
+var obj ={
+    name:"sai",
+    age:19
+}
+console.log(obj);
+console.log(typeof obj);
+
+//for loop
+for(let i=0;i<=10;i++)
+{
+    console.log(i);
+}
+//while loop
+let i=1;
+while(i<=10)
+{
+    console.log(i);
+    i++;
+}
+//do while loop
+i=1;
+do{
+    console.log('do..while');
+}
+while(i==2);
+//if else
+//siwtch
+//ternary operator*/
+
+
+//multiple conditions
+/*mark=20;
+let res =(mark>=90)?"O grade":(mark>=80)?"A grade":(mark>=70)?"B grade":(mark>=35)?"pass":"fail";
+console.log(res);*/
+//funtion
+var add =(a,b)=>
+{
+    console.log(a+b);
+}
+add(10,20)
+console.log(z);
+var z;
+
+//spread operator
+var arr1=[10,20,30];
+var arr2=[...arr1,40,50,60];
+console.log(arr2);
+
+//destructuring operator
+var[m1,m2,m3,m4,m5]=[89,90,91,92,93];
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(m4);
+console.log(m5);
+
+//deconstructing object
+var{name,age,dept}=
+{
+    name:"sai",
+    age:19,
+    dept:"cs",
+}
+console.log(name);
+console.log(age);
+console.log(dept);
